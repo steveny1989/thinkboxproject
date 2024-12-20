d1743bc58a22fba1878cdfdf5be8e2c043b46ceb
steveny1989 <steveny1989@gmail.com>
2024-12-20 19:00:49 +0800

    Update activity.js

--- js/activity.js
+++ js/activity.js
@@ -6,7 +6,7 @@ const token = localStorage.getItem('jwtToken');
 // 检查 token 是否存在，如果不存在，提示用户登录
 if (!token) {
     alert('Please log in first.');
-    window.location.href = '/newAuth.html';  // 跳转到登录页面
+    window.location.href = '../newAuth.html';  // 跳转到登录页面
 } else {
     // 发送请求时附加 Authorization 头
     fetch('https://api.thinkboxs.com/activity/activity-stats', {
